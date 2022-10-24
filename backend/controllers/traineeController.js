e7b229e004deb344329e4f011830dbafe2073e99
unknown <adhamaldeib@gmail.com>
2022-10-24 21:33:13 +0200

    view all courses

--- backend/controllers/traineeController.js
+++ backend/controllers/traineeController.js
@@ -1,6 +1,7 @@
 const Trainee = require('../models/traineeModel')
 
 const mongoose = require('mongoose')
+const Course = require('../models/courseModel')
 
 const createTrainee = async (req,res) => {
     const {Name,Email,Age} = req.body
@@ -20,25 +21,25 @@ const getTrainees = async (req,res) => {
 const getTrainee = async (req,res) => {
     const{ id } = req.params
     if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'no such trainee'})
+        return res.status(404).json({error: 'no such trainee1'})
     }
 
     const trainee = await Trainee.findById(id)
     if(!trainee){
-        return res.status(404).json({error: 'no such trainee'})
+        return res.status(404).json({error: 'no such trainee2'})
     }
     res.status(200).json(trainee)
 }
 const deleteTrainee = async (req,res) => {
     const{ id } = req.params
     if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'no such trainee'})
+        return res.status(404).json({error: 'no such trainee3'})
     }
     const trainee = await Trainee.findByIdAndDelete(id)
     //or you can use Workout.findOneAndDelete({_id: id})
 
     if(!trainee){
-        return res.status(404).json({error: 'no such trainee'})
+        return res.status(404).json({error: 'no such trainee4'})
     }
     res.status(200).json(trainee)
 }
@@ -47,18 +48,33 @@ const updateTrainee = async (req,res) => {
     const{ id } = req.params
 
     if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'no such trainee'})
+        return res.status(404).json({error: 'no such trainee5'})
     }
     const trainee = await Trainee.findOneAndUpdate(id,req.body)
     if(!trainee){
-        return res.status(404).json({error: 'no such trainee'})
+        return res.status(404).json({error: 'no such trainee6'})
     }
     res.status(200).json(trainee)
 }
+
+const viewAllCourses = async (req,res) => {
+   // try {
+        const courses = await Course.find({})
+        if(!courses){
+            return res.status(404).json({error: 'no courses found'})
+        }
+            res.status(200).json(courses)
+        
+    // } catch (error) {
+    //     res.status(400).json({error: 'no courses found'})
+    // }
+}
+
 module.exports = {
     getTrainees,
     getTrainee,
     createTrainee,
     deleteTrainee,
-    updateTrainee
+    updateTrainee,
+    viewAllCourses
 }
