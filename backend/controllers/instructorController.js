f806457a8b20e41360beaed894bc8b67d554fe6e
Youssef Bahei <youssefbahei@Youssefs-MacBook-Pro.local>
2022-10-28 19:18:55 +0200

    frontend price

--- backend/controllers/instructorController.js
+++ backend/controllers/instructorController.js
@@ -1,4 +1,4 @@
-zconst Instructor = require('../models/instructorModel')
+const Instructor = require('../models/instructorModel')
 const Course = require('../models/courseModel')
 const mongoose = require('mongoose')
 
@@ -15,9 +15,16 @@ const createCourse = async (req, res) => {
 }
 
 const searchCourse = async (req, res) => {
-    const { id } = req.params
-
-    const course = await Course.find({ InstructorId: { id } }).find(req.body)
+    const { InstructorId } = req.body
+    const { Title } = req.body
+    const { Subject } = req.body
+    var course
+    if (Title == null && Subject != null)
+        course = await Course.find({ InstructorId }).find({ Subject })
+    else if (Subject == null && Title != null)
+        course = await Course.find({ InstructorId }).find({ Title })
+    else
+        course = await Course.find({ InstructorId })
     if (!course) {
         return res.status(404).json({ error: 'no such course' })
     }
@@ -39,10 +46,24 @@ const filterCourse = async (req, res) => {
     res.status(200).json(course)
 }
 
+const filterCoursePrice = async (req, res) => {
+    try {
+        const { priceMin, priceMax } = req.body
+        const courses = await Course.find({ Price: { $gte: priceMin } && { $lte: priceMax } })
+        if (!courses) {
+            return res.status(404).json({ error: 'no courses found' })
+        }
+        res.status(200).json(courses)
+    } catch (error) {
+        res.status(400).json({ error: 'error' })
+    }
+}
+
 
 
 module.exports = {
     searchCourse,
     createCourse,
-    filterCourse
+    filterCourse,
+    filterCoursePrice
 }
