0ea9e9430013598ec4eca27840fb9f569eb63377
Karim <karimouf00@gmail.com>
2022-11-02 00:19:15 +0200

    Merge branch 'main' of https://github.com/Advanced-Computer-Lab-2022/ACL-Crocodiles

--- frontend/src/components/SearchDetails.js
+++ frontend/src/components/SearchDetails.js
@@ -1,11 +1,31 @@
 const SearchDetails = ({ course }) => {
     return(
+        
         <div className="search-details">
             <h4>{course.Title}</h4>
             <p><strong>Subject:</strong>{course.Subject}</p>    
             <p><strong>Hours:</strong>{course.Hours}</p>
+            <span className="viewmore">
+            <p><strong>Price:</strong>{course.Price}</p> 
+            <p><strong>Discount:</strong>{course.Discount}</p> 
+            <p><strong>Subtitles:</strong>{course.Subtitle}</p>
+            </span>
+            <button className="more" onClick="viewMore()" >More Details</button>
         </div>
     )
+    function viewMore(){
+        console.log("karim")
+        var moreText = document.getElementById("viewmore");
+        var btnText = document.getElementById("more");
+      
+        if (btnText.innerHTML = "More Details") {
+          btnText.innerHTML = "Less Details ";
+          moreText.style.display = "none";
+        } else {
+          btnText.innerHTML = "less Details";
+          moreText.style.display = "inline"
+          }
+        }
 }
 
 export default SearchDetails
