1bd229cc5f9b46a68c6fa6f3d3bfd2f2b63102a4
Nayer Kotry <nayer.kotry@gmail.com>
2022-12-03 01:03:41 +0200

    fixed view all courses (trainee)

--- frontend/src/pages/Course.js
+++ frontend/src/pages/Course.js
@@ -1,16 +1,23 @@
 import {useEffect,useState} from 'react'
 
 import DropDown from '../components/CountryDropDown'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 //import NewCourseForm from '../components/NewCourseForm'
 const Course = () => {
+    const {user} = useAuthContext();
     const [courses,setCourses] = useState(null)
     const[error,setError]=useState(null)
 
 
     useEffect(() => {
         const fetchCourses = async () => {
-            const response = await fetch('/api/trainee/page/viewAllCourses')
+            const response = await fetch('/api/trainee/page/viewAllCourses',{
+                method: 'GET',
+                headers:{
+                    'Authorization':`Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
             if(response.ok){
                 setCourses(json)
